236b45338566466a7fed178f2b6b21b1d717abbe
MdNazmul9 <nazmul.cse.pust@gmail.com>
2020-12-05 19:49:26 +0600

    db connect

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Tweet
 
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
+    #print(args, kwargs)
     return HttpResponse("<h1>Hello world <h1>")
 
 def home_Detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id} <h1>")
+    #print(args, kwargs)
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+
+    except:
+        raise Http404
+
+    
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} <h1>")   
