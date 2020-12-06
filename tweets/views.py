f2dd7c2986edf8d1872332bf01f766647fd593fa
MdNazmul9 <nazmul.cse.pust@gmail.com>
2020-12-06 10:45:44 +0600

    json view

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpResponse, Http404
+
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 def home_view(request, *args, **kwargs):
@@ -10,11 +10,29 @@ def home_view(request, *args, **kwargs):
 
 def home_Detail_view(request, tweet_id, *args, **kwargs):
     #print(args, kwargs)
+    '''
+
+    REST API VIEW
+    Consume by Javascript or Swift /java/iOS/Android
+    return json data
+
+    '''
+    
+    data = {
+
+        "id": tweet_id,
+      
+    }
+
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
 
     except:
-        raise Http404
+        #raise Http404
+        data['meaasge'] = "Not Found"
+        status = 404
 
-    
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} <h1>")   
+    #return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} <h1>")
+    return JsonResponse(data , status=status)   # json.dumps content_type = 'application/json'
