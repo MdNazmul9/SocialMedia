61897143b8cd15563b33dbf94ce8b4a604f26192
MdNazmul9 <nazmul.cse.pust@gmail.com>
2021-01-25 13:21:06 +0600

    ajax response ok

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import random
+
 # Create your models here.
 
 class Tweet(models.Model):
@@ -7,3 +9,10 @@ class Tweet(models.Model):
     #id = models.AutoField(primary_key=True)
     content = models.TextField()
     #image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "content":self.content,
+            "likes":random.randint(100,1000)
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -29,7 +29,7 @@ def tweet_create_view(request, *args, **kwargs):
         obj.save()
 
         if request.is_ajax():
-            return JsonResponse({},status=201) # 201 for created items
+            return JsonResponse(obj.serialize(), status=201) # 201 for created items
         
 
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
@@ -43,7 +43,9 @@ def tweet_create_view(request, *args, **kwargs):
 
 def Tweet_LIstView (request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id":x.id, "content":x.content, "likes":random.randint(0,1000)} for x in qs]
+    #tweets_list = [{"id":x.id, "content":x.content, "likes":random.randint(0,1000)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
+    
     data = {
         "isUser":False,
         "response": tweets_list
