be7a9746bc72251fbb8e43521cc3aba4d812485d
MdNazmul9 <nazmul.cse.pust@gmail.com>
2021-01-25 15:47:56 +0600

    handling form error

--- tweets/models.py
+++ tweets/models.py
@@ -8,11 +8,14 @@ class Tweet(models.Model):
     #SQL complecated
     #id = models.AutoField(primary_key=True)
     content = models.TextField()
-    #image = models.FileField(upload_to='images/', blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
 
     def serialize(self):
         return {
             "id":self.id,
             "content":self.content,
             "likes":random.randint(100,1000)
-        }
+        }  
